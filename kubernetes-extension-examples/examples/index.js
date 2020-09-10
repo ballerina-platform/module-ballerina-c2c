243b59c048e31f422fbce15d9bd362b2d93cfc7a
anuruddhal <anuruddhal@wso2.com>
2020-09-10 09:25:50 +0530

    Fix build failures due to stdlib migration

--- kubernetes-extension-examples/examples/index.js
+++ kubernetes-extension-examples/examples/index.js
@@ -5,18 +5,6 @@
         "category": "Language concepts",
         "samples": [
             {
-                "name": "Kubernetes Deployment",
-                "url": "kubernetes-deployment"
-            },
-            {
-                "name": "OpenShift Deployment",
-                "url": "openshift-deployment"
-            },
-            {
-                 "name": "Knative Deployment",
-                 "url": "knative-deployment"
-            },
-            {
                 "name": "Kubernetes Deployment without Annotations",
                 "url": "kubernetes-deployment-without-annotations"
             }
