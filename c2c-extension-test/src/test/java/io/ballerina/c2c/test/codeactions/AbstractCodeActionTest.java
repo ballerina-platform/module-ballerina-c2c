e3aef335b429a51378226c03f0fc14078151432e
Anjana Supun <anjanas@wso2.com>
2021-12-10 10:16:15 +0530

    Add missing import

--- c2c-extension-test/src/test/java/io/ballerina/c2c/test/codeactions/AbstractCodeActionTest.java
+++ c2c-extension-test/src/test/java/io/ballerina/c2c/test/codeactions/AbstractCodeActionTest.java
@@ -44,6 +44,7 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
